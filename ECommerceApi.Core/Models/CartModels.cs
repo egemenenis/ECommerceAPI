f5f46c082d33c48a8f1de4fb8a7b4319e083f56a
Egemen Enis Ertaş <84728316+egemenenis@users.noreply.github.com>
2024-09-23 17:00:42 +0300

    Cart fetching and creation endpoint

---
+++ ECommerceApi.Core/Models/CartModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApi.Core.Models
+{
+    public class CartModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsClosed { get; set; }
+        public int AccountId { get; set; }
+    }
+}
---
+++ ECommerceApi/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using ECommerceApi.API.DataAccess;
+using ECommerceApi.API.Entites;
+using ECommerceApi.Core.Controllers;
+using ECommerceApi.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class CartController : ControllerBase
+    {
+        private DatabaseContext _db;
+        private IConfiguration _configuration;
+
+        public CartController(DatabaseContext databaseContext, IConfiguration configuration)
+        {
+            _db = databaseContext;
+            _configuration = configuration;
+        }
+
+
+        //GetOrCreate => Bring or create a cart
+        //AddToCart => Adding product to cart
+
+        [HttpGet("GetOrCreate/{accountId}")]
+        [ProducesResponseType(200, Type = typeof(Resp<CartModel>))]
+        public IActionResult GetOrCreate([FromRoute] int accountId)
+        {
+            Resp<CartModel> response = new Resp<CartModel>();
+            Cart cart = _db.Carts.SingleOrDefault(x => x.AccountId == accountId && x.IsClosed == false);
+
+            if (cart == null)
+            {
+                cart = new Cart
+                {
+                    AccountId = accountId,
+                    Date = System.DateTime.Now,
+                    IsClosed = false
+                };
+
+                _db.Carts.Add(cart);
+                _db.SaveChanges();
+            }
+
+            CartModel data = new CartModel
+            {
+                Id = cart.Id,
+                AccountId = cart.AccountId,
+                Date = cart.Date,
+                IsClosed = cart.IsClosed
+            };
+
+            response.Data = data;
+            return Ok(response);
+        }
+    }
+}
